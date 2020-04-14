0ad75872fb024ccf2473c9f683f1c96817ea9169
apilo <apilo@HP-ZBook-Cmontmo-Andre>
2020-04-14 10:56:37 -0400

    Affichage du gabarit seulement pendant la phase de tests

--- index.php
+++ index.php
@@ -4,4 +4,6 @@
 
 //$routeur = new Routeur();
 //$routeur->routerRequete();
+$titre = "TESTS des modèles et des vues";
+$contenu = '<span style="color : red;">Affichage du gabarit seulement pendant la phase de tests.</span>';
 require 'Vue/gabarit.php';
